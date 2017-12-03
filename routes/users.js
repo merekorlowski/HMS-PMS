244884be6b606b7c605afce3d129238b9819a9c0
patrick <patrick.as@live.com>
2017-12-02 19:25:26 -0500

    add SQL to get user

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,43 @@
 const express = require('express')
 const router = express.Router()
 
+
 router.get('/users', (req, res) => {
   //get users from db
   return null
 })
 
+// DONE: PAS 
+ 
 router.get('/user', (req, res) => {
   //get user from db
+
+  pg.connect(connectionString, (err, client, done) => {
+
+		// Handle connection errors
+		if(err) {
+			done();
+			console.log(err);
+			return res.status(500).json({success: false, data: err});
+		}
+
+		//Add a NextOfKin
+		let queryText = `
+			SELECT * from HMS-PMS.Staff 
+			WHERE 
+			userID = '${req.body.roomNumber}'
+			AND 
+			password = '${req.body.roomNumber}';
+			`;
+
+		const query = client.query(queryText);
+
+		// After all data is returned, close connection and return results
+		query.on('end', () => {
+			done();
+			return res.json();
+		});
+	});
   return null
 })
 
